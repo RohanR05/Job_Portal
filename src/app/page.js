6674e96f50dff4277b2d0e5c68e31bac25dd5274
003-demo <rrriku05@gmail.com>
2025-11-04 23:42:46 +0600

    install daisyui

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>h</div>;
+  return <div>Landing page</div>;
 }
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
     <div>
       Navbar
+      <button className="btn btn-outline bg-red-100">yes</button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
