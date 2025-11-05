a58b7513b1699432f37b6a2d86162f3265d7e410
003-demo <rrriku05@gmail.com>
2025-11-05 22:36:27 +0600

    logo complete

--- src/components/shared/Logo.jsx
+++ src/components/shared/Logo.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Logo = () => {
   return (
     <div>
-      <h2>Intern?</h2>
+      <Link href={"/"}>
+        <h2 className="text-2xl font-semibold md:text-3xl lg:text-4xl md:font-bold text-primary">
+          Intern<span className="text-secondary">Bangla</span>
+        </h2>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
