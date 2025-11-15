9c4c933462e35ac1b2f6ef5d6f55ae475d4f5866
003-demo <rrriku05@gmail.com>
2025-11-15 23:39:16 +0600

    applypage

--- src/app/applyJob/page.jsx
+++ src/app/applyJob/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ApplyPage = () => {
-  return <div className="mt-16 md:mt-18">df</div>;
+  return (
+    <div className="mt-16 md:mt-18 flex items-center justify-center h-screen">
+      <h2 className="text-5xl font-bold">Under Developement</h2>
+    </div>
+  );
 };
 
 export default ApplyPage;
