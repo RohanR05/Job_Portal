08c48fa06861d3cfc320614fff9e7a8348fa7017
003-demo <rrriku05@gmail.com>
2025-11-15 23:42:13 +0600

    dashboard layout

--- src/app/(dashBoard)/dashBoard/page.jsx
+++ src/app/(dashBoard)/dashBoard/page.jsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-  return <div>DashBoard</div>;
+  return (
+    <div className="flex flex-col gap-5 items-center justify-center h-screen">
+      <h1 className="text-5xl">DashBoard Layout</h1>
+      <h2 className="text-2xl">Under Developerment</h2>
+      <button className="btn btn-outline btn-primary">
+        <Link href={"/"}>Home</Link>
+      </button>
+    </div>
+  );
 };
 
 export default page;
