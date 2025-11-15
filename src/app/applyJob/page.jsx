79efd46bf11e406bed450538ecdc8fd5f9303773
003-demo <rrriku05@gmail.com>
2025-11-15 23:44:43 +0600

    apply button back

--- src/app/applyJob/page.jsx
+++ src/app/applyJob/page.jsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 const ApplyPage = () => {
   return (
-    <div className="mt-16 md:mt-18 flex items-center justify-center h-screen">
-      <h2 className="text-5xl font-bold">Under Developement</h2>
+    <div className="mt-16 md:mt-18 flex items-center justify-center h-screen flex-col gap-6">
+      <h1 className="text-5xl font-bold">Apply Page</h1>
+      <h2 className="text-2xl font-bold">Under Developement</h2>
+      <button className="btn btn-primary btn-outline">
+        <Link href={"/"}>Home</Link>
+      </button>
     </div>
   );
 };
